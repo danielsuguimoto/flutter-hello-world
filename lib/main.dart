a5c205ce2b43c4392814b24cdb65d98eaaa80128
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2021-03-14 14:44:52 -0300

    adding interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,100 @@ class HelloApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Hello World',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Hello World'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.red,
       ),
+      home: RandomWords(),
     );
   }
 }
 
 class RandomWords extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  RandomWordsState createState() => RandomWordsState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
+class RandomWordsState extends State<RandomWords> {
+  final suggestions = <WordPair>[];
+  final saved = <WordPair>[];
+  final biggerFont = TextStyle(fontSize: 18.0);
+
+  void pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          final tiles = saved.map((WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: biggerFont,
+              ),
+            );
+          });
+
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Saved Suggestions"),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
-    Widget build(BuildContext context) {
-      final wordPair = WordPair.random();
-      return Text(wordPair.asPascalCase);
-    }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: pushSaved),
+        ],
+      ),
+      body: buildSuggestions(),
+    );
+  }
+
+  Widget buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+          final index = i ~/ 2; /*3*/
+          if (index >= suggestions.length) {
+            suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return buildRow(suggestions[index]);
+        });
+  }
+
+  Widget buildRow(WordPair pair) {
+    final isSaved = saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: biggerFont,
+      ),
+      trailing: Icon(
+        isSaved ? Icons.favorite : Icons.favorite_border,
+        color: isSaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (isSaved) {
+            saved.remove(pair);
+          } else {
+            saved.add(pair);
+          }
+        });
+      },
+    );
+  }
 }
